c670916955fb053ebb2a12740d5a25f743577459
Kun Ma <kunma@fortinet.com>
2016-02-24 10:53:10 -0800

    Trial commit

--- registration.js
+++ registration.js
@@ -11,9 +11,12 @@ describe('my webdriverio tests', function(){
             client.init(done);
     });
 
-    it('Login',function(done) {
+    it('should open CTC page', function() {
+        return client.url('http://student3-test.cedartc.org/').pause(500);
+    });
+
+    it('should login the CTC home page', function(done) {
         client
-            .url('http://student3-test.cedartc.org/')
             .setValue('#Email', 's9901@cedartc.org')
             .pause(1000)
             .click('#next')
@@ -21,13 +24,35 @@ describe('my webdriverio tests', function(){
             .setValue('#Passwd', 'ctc_9901')
             .pause(1000)
             .click('#signIn')
-            .pause(1000)
-            .setValue('#debugUserId', 's9901')
-            .refresh()
-            .call(done);
-
+            .pause(500);
     });
 
+    it('should load correct title', function() {
+        return 
+            client
+                .getTitle(function(err, title) {
+                    assert(err === undefined);
+                    assert(title === '柏訓學員網');
+                    console.log(title);
+                });
+    });
+
+/*    it('Change debug user', function(done) {
+        client
+            .getTitle(function(err, title) {
+                assert(err === undefined);
+                assert(title === '柏訓學員網');
+            });
+            .setValue('#debugUserId', 's9901')
+            .pause(1000)
+
+            .getValue('#debugUserId').then (function(e) {
+                assert(e === 's9901');
+                console.log("Debug user ID: " + e);
+            })
+            .call(done);
+    });
+*/
     after(function(done) {
         client.end(done);
     });
