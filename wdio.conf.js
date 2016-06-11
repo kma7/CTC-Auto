4985cc416c96e361cb453e904f1f9c7b0498b9b4
kma7 <makun211@gmail.com>
2016-06-11 14:45:03 -0700

    With latest WebDriverIO version

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        'test.js'
+        './test/specs/**/mobile.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -20,14 +20,17 @@ exports.config = {
     // ============
     // Capabilities
     // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilties at the same
+    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
     // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude option in
+    // sessions. Within your capabilities you can overwrite the spec and exclude options in
     // order to group specific specs to a specific capability.
     //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://docs.saucelabs.com/reference/platforms-configurator
+    // First, you can define how many instances should be started at the same time. Let's
+    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // files and you set maxInstances to 10, all spec files will get tested at the same time
+    // and 30 processes will get spawned. The property handles how many capabilities
+    // from the same test should run tests.
     //
     capabilities: [{
         browserName: 'chrome'
@@ -38,7 +41,12 @@ exports.config = {
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
     //
-    // Level of logging verbosity.
+    // By default WebdriverIO commands are executed in a synchronous way using
+    // the wdio-sync package. If you still want to run your tests in an async way
+    // e.g. using promises you can set the sync option to false.
+    sync: true,
+    //
+    // Level of logging verbosity: silent | verbose | command | data | result | error
     logLevel: 'verbose',
     //
     // Enables colors for log output.
@@ -48,14 +56,21 @@ exports.config = {
     screenshotPath: './errorShots/',
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
-    // with "/", the base url gets prepended.
-    baseUrl: 'http://student3-test.cedartc.org/',
+    // with "/", then the base url gets prepended.
+    baseUrl: 'http://localhost/',
     //
-    // Default timeout for all waitForXXX commands.
-    waitforTimeout: 10000,
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: 100000,
+    //
+    // Default timeout in milliseconds for request
+    // if Selenium Grid doesn't send response
+    connectionRetryTimeout: 90000,
+    //
+    // Default request retries count
+    connectionRetryCount: 3,
     //
     // Initialize the browser instance with a WebdriverIO plugin. The object should have the
-    // plugin name as key and the desired plugin options as property. Make sure you have
+    // plugin name as key and the desired plugin options as properties. Make sure you have
     // the plugin installed before running any tests. The following plugins are currently
     // available:
     // WebdriverCSS: https://github.com/webdriverio/webdrivercss
@@ -72,21 +87,27 @@ exports.config = {
     //     browserevent: {}
     // },
     //
+    // Test runner services
+    // Services take over a specific job you don't want to take care of. They enhance
+    // your test setup with almost no effort. Unlike plugins, they don't add new
+    // commands. Instead, they hook themselves up into the test process.
+  /*  services: ['sauce'],
+    user: 'kun_ma',
+    key: '362d2946-1fd1-47a7-a586-5d57e18e002d',
+    sauceConnect: false,*/
+    //
     // Framework you want to run your specs with.
-    // The following are supported: mocha, jasmine and cucumber
+    // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
     //
-    // Make sure you have the node package for the specific framework installed before running
-    // any tests. If not please install the following package:
-    // Mocha: `$ npm install mocha`
-    // Jasmine: `$ npm install jasmine`
-    // Cucumber: `$ npm install cucumber`
+    // Make sure you have the wdio adapter package for the specific framework installed
+    // before running any tests.
     framework: 'mocha',
     //
     // Test reporter for stdout.
-    // The following are supported: dot (default), spec and xunit
+    // The following are supported: dot (default), spec, and xunit
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    reporter: 'spec',
+    reporters: ['dot'],
     
     //
     // Options to be passed to Mocha.
@@ -94,34 +115,65 @@ exports.config = {
     mochaOpts: {
         ui: 'bdd'
     },
-    
     //
     // =====
     // Hooks
     // =====
-    // Run functions before or after the test. If one of them returns with a promise, WebdriverIO
-    // will wait until that promise got resolved to continue.
+    // WedriverIO provides several hooks you can use to interfere with the test process in order to enhance
+    // it and to build services around it. You can either apply a single function or an array of
+    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+    // resolved to continue.
     //
-    // Gets executed before all workers get launched.
-    onPrepare: function() {
-        // do something
-    },
+    // Gets executed once before all workers get launched.
+    // onPrepare: function (config, capabilities) {
+    // },
     //
-    // Gets executed before test execution begins. At this point you will have access to all global
-    // variables like `browser`. It is the perfect place to define custom commands.
-    before: function() {
-        // do something
-    },
+    // Gets executed before test execution begins. At this point you can access all global
+    // variables, such as `browser`. It is the perfect place to define custom commands.
+    // before: function (capabilities, specs) {
+    // },
+    //
+    // Hook that gets executed before the suite starts
+    // beforeSuite: function (suite) {
+    // },
+    //
+    // Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+    // beforeEach in Mocha)
+    // beforeHook: function () {
+    // },
+    //
+    // Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+    // afterEach in Mocha)
+    // afterHook: function () {
+    // },
+    //
+    // Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+    // beforeTest: function (test) {
+    // },
+    //
+    // Runs before a WebdriverIO command gets executed.
+    // beforeCommand: function (commandName, args) {
+    // },
+    //
+    // Runs after a WebdriverIO command gets executed
+    // afterCommand: function (commandName, args, result, error) {
+    // },
+    //
+    // Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+    // afterTest: function (test) {
+    // },
+    //
+    // Hook that gets executed after the suite has ended
+    // afterSuite: function (suite) {
+    // },
     //
     // Gets executed after all tests are done. You still have access to all global variables from
     // the test.
-    after: function(failures, pid) {
-        // do something
-    },
+    // after: function (capabilities, specs) {
+    // },
     //
     // Gets executed after all workers got shut down and the process is about to exit. It is not
     // possible to defer the end of the process using a promise.
-    onComplete: function() {
-        // do something
-    }
-};
+    // onComplete: function(exitCode) {
+    // }
+}
